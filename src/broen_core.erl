35b22eb0661809622a194b0b6e4b76abed09478f
Pawel Antemijczuk <pan@issuu.com>
2018-10-22 13:00:09 +0200

    Fix cookie stuff

--- src/broen_core.erl
+++ src/broen_core.erl
@@ -135,8 +135,19 @@ handle(Req0, Exchange, CookiePath, Options) ->
                        Req0);
     {Origin, OriginMode} ->
       {AmqpRes, ExtraCookies} = amqp_call(Req0, Exchange, RoutingKey, Timeout),
-      ReqWithCookies = lists:foldl(fun({Name, Value}, R) -> cowboy_req:set_resp_cookie(Name, Value, R) end,
-                                   Req0, ExtraCookies),
+      io:format("Extra cookies: ~p~n", [ExtraCookies]),
+      ReqWithCookies = lists:foldl(fun({CookieName, CookieValue}, R) ->
+
+        Value = maps:get(value, CookieValue),
+        MaxAge = maps:get(max_age, CookieValue),
+        Path = maps:get(path, CookieValue, <<"/">>),
+        Domain = maps:get(domain, CookieValue),
+
+        cowboy_req:set_resp_cookie(CookieName, Value, R, #{
+          domain => Domain,
+          path => Path,
+          max_age => MaxAge
+        }) end, Req0, ExtraCookies),
       case AmqpRes of
         {ok, PackedResponse, ContentType} ->
           {ok, SerializerMod} = application:get_env(broen, serializer_mod),
@@ -319,7 +330,7 @@ parse_date(Date) ->
     iso8601:parse(Date)
   catch
     _:badarg ->
-      stringdate_to_datetime(Date)
+      cow_date:parse_date(Date)
   end.
 
 %% Other
@@ -353,35 +364,3 @@ histogram_notify(Name, Diff) ->
     Res ->
       Res
   end.
-
-stringdate_to_datetime([$ |T]) ->
-  stringdate_to_datetime(T);
-stringdate_to_datetime([_D1, _D2, _D3, $\,, $ |Tail]) ->
-  stringdate_to_datetime1(Tail).
-
-stringdate_to_datetime1([A, B, $\s |T]) ->
-  stringdate_to_datetime2(T, erlang:list_to_integer([A,B]));
-stringdate_to_datetime1([A, $\s |T]) ->
-  stringdate_to_datetime2(T, erlang:list_to_integer([A])).
-
-stringdate_to_datetime2([M1, M2, M3, $\s , Y1, Y2, Y3, Y4, $\s,
-                         H1, H2, $:, Min1, Min2,$:,
-                         S1, S2,$\s ,$G, $M, $T|_], Day) ->
-  {{erlang:list_to_integer([Y1,Y2,Y3,Y4]),
-    month_str_to_int([M1, M2, M3]), Day},
-   {erlang:list_to_integer([H1, H2]),
-    erlang:list_to_integer([Min1, Min2]),
-    erlang:list_to_integer([S1, S2])}}.
-
-month_str_to_int("Jan") -> 1;
-month_str_to_int("Feb") -> 2;
-month_str_to_int("Mar") -> 3;
-month_str_to_int("Apr") -> 4;
-month_str_to_int("May") -> 5;
-month_str_to_int("Jun") -> 6;
-month_str_to_int("Jul") -> 7;
-month_str_to_int("Aug") -> 8;
-month_str_to_int("Sep") -> 9;
-month_str_to_int("Oct") -> 10;
-month_str_to_int("Nov") -> 11;
-month_str_to_int("Dec") -> 12.
